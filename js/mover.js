c0a9eb7bf62bd89a15599699f24f9355a7380015
Diego Martínez Alaminos <55781947+diegomartinezalaminos@users.noreply.github.com>
2019-12-06 14:31:03 +0100

    Add files via upload

--- js/mover.js
+++ js/mover.js
@@ -1,8 +1,12 @@
+
+//Esta función es la encargada de que vuelva toto a su sitio
 function IzquierdaDerecha(x){
+    //Estas dos frases buscan el elemato con la id = "x" y remplazan una de sus clases (izquierda o derecha) por otra que se le especifique (IzquierdaDerecha)
     document.getElementById(x).className = document.getElementById(x).className.replace( /(?:^|\s)izquierda(?!\S)/g , 'IzquierdaDerecha' );
     document.getElementById(x).className = document.getElementById(x).className.replace( /(?:^|\s)derecha(?!\S)/g , 'IzquierdaDerecha' );
 }
-
+//Esta función es la encargada de que aparezcan los elementos
 function aparecer(x){
+    //Esta frases buscan el elemato con la id = "x" y remplazan una de sus clases (opacidad0) por otra que se le especifique (opacidad1)
     document.getElementById(x).className = document.getElementById(x).className.replace( /(?:^|\s)opacidad0(?!\S)/g , 'opacidad1' );
 }
